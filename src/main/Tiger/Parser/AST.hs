d8e47a7355e4906503ae048fdddef37f282d16d5
TheBizzle <jason.bertsche@gmail.com>
2026-06-14 14:20:30 -0500

    Prefactor: Expose proper API from `AST`

--- src/main/Tiger/Parser/AST.hs
+++ src/main/Tiger/Parser/AST.hs
@@ -1,4 +1,17 @@
 {-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
 module Tiger.Parser.AST(module Tiger.Parser.Internal.AST) where
 
-import Tiger.Parser.Internal.AST(Expr, Symbol(Symbol))
+import Tiger.Parser.Internal.AST(
+    Decl(FunctionDecl, TypeDecl, VariableDecl)
+  , Expr( ArrayExpr, AssignExpr, BreakExpr, CallExpr, ForExpr, IfExpr, IntExpr, LetExpr, LValueExpr, NilExpr
+        , OpExpr, RecordExpr, SeqExpr, StringExpr, token, WhileExpr)
+  , Field(Field, fieldName)
+  , FuncDecl(FuncDecl)
+  , LValue(ArrayIndex, Variable, RecordField)
+  , Operator(DivideOp, EqualsOp, GreaterOrEqualsOp, GreaterThanOp, LessOrEqualsOp, LessThanOp, MinusOp
+            , NotEqualsOp, PlusOp, TimesOp)
+  , Symbol(Symbol, symbolText)
+  , Type(ArrayType, NamedType, RecordType)
+  , TypeDeclEntry(TypeDeclEntry, typeDeclName, typeDeclToken)
+  , VarDecl(VarDecl)
+  )
